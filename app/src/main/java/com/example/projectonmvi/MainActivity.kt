0e064d459dea929c1204ea6ca2650d174d2aba22
ArsenKoch <arsenkocaran@icloud.com>
2022-12-27 23:45:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/projectonmvi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectonmvi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
